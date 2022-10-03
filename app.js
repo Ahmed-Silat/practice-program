45bcf56e4ad1837ba244d20335b27a757349a313
Ahmed-Silat <ahmedsilat@gmail.com>
2022-10-04 01:17:33 +0500

    second commit

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
 // ATM Machine Program
 var amount = Number(prompt("Enter the amount you want to withdraw"));
-var notesOf5000 = parseInt(amount / 5000) - 1;
-var notesOf1000 = parseInt((((amount % 5000) + 1) * 5000) / 1000);
+var notesOf5000;
+var notesOf1000;
 var notesOf500 = parseInt((notesOf1000 % 1000) / 500);
-var fiveThousand = 0;
-var oneThousand = 0;
-var fiveHundred = 0;
+// var fiveThousand = 0;
+// var oneThousand = 0;
+// var fiveHundred = 0;
 if (amount % 500 === 0 && amount >= 0 && amount <= 20000) {
   if (amount % 5000 === 0) {
-    fiveThousand = notesOf5000;
-    oneThousand = notesOf1000;
-    fiveHundred = 1000 / 500;
+    notesOf5000 = parseInt(amount / 5000) - 1;
+    notesOf1000 = parseInt((((amount % 5000) + 1) * 5000) / 1000) - 1;
+    notesOf500 = 1000 / 500;
   } else if (amount % 5000 !== 0 && amount % 1000 === 0) {
-    fiveThousand = notesOf5000;
-    oneThousand = notesOf1000;
-    fiveHundred = notesOf500;
+    notesOf5000 = parseInt(amount / 5000) - 1;
+    notesOf1000 = parseInt((amount - (notesOf5000 * 5000)) / 1000) - 1;
+    notesOf500 = 1000 / 500;
   } else {
-    fiveThousand = notesOf5000;
-    oneThousand = notesOf1000;
-    fiveHundred = notesOf500;
+    notesOf5000 = parseInt(amount / 5000) - 1;
+    notesOf1000 = parseInt((amount - (notesOf5000 * 5000)) / 1000) - 1;
+    notesOf500 = notesOf500;
   }
   document.write(
     "<h3>" +
       "You will have " +
-      fiveThousand +
+      notesOf5000 +
       " fiveThousand notes " +
-      oneThousand +
+      notesOf1000 +
       " oneThousand notes " +
-      fiveHundred +
+      notesOf500 +
       " fiveHundred notes " +
       "</h3>"
   );
