970fa466810c8e5b8d320d9f31642c5dceac189c
Ahmed-Silat <ahmedsilat@gmail.com>
2022-10-04 17:58:02 +0500

    practice

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 // ATM Machine Program
-/*
+
 var amount = Number(prompt("Enter the amount you want to withdraw", "0"));
 var notesOf5000;
 var notesOf1000;
-var notesOf500 = parseInt((notesOf1000 % 1000) / 500);
+var notesOf500;
 // var fiveThousand = 0;
 // var oneThousand = 0;
 // var fiveHundred = 0;
@@ -47,7 +47,7 @@ if (amount % 500 === 0 && amount > 0 && amount <= 20000) {
 } else {
   document.write("<h2>Invalid Amount</h2>");
 }
-*/
+
 
 // Custom indexOf method
 /*
@@ -64,6 +64,7 @@ console.log(customIndexOf("r"));
 */
 
 // Vowel Program
+/*
 var text = "Please read this application and give me gratuity";
 var array = text.split(" ");
 console.log(array);
@@ -105,3 +106,4 @@ for (var i = 0; i < pairOfVowels.length; i++) {
   }
 }
 console.log(setOfVowels);
+*/
