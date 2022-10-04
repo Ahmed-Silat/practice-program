bd6f53588f8c8d688a7f2624b73ba46cac7887a5
Ahmed-Silat <ahmedsilat@gmail.com>
2022-10-04 11:12:15 +0500

    fifth commit

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // ATM Machine Program
+/*
 var amount = Number(prompt("Enter the amount you want to withdraw", "0"));
 var notesOf5000;
 var notesOf1000;
@@ -10,16 +11,16 @@ if (amount % 500 === 0 && amount > 0 && amount <= 20000) {
   if (amount % 5000 === 0) {
     notesOf5000 = parseInt(amount / 5000) - 1;
     notesOf1000 = parseInt((((amount % 5000) + 1) * 5000) / 1000) - 1;
-    notesOf500 = 1000 / 500;
+    notesOf500 = (amount - notesOf5000 * 5000 - notesOf1000 * 1000) / 500;
   } else if (amount % 5000 !== 0 && amount % 1000 === 0) {
     if (amount < 5000) {
       notesOf5000 = parseInt(amount / 5000);
       notesOf1000 = parseInt((amount - notesOf5000 * 5000) / 1000) - 1;
-      notesOf500 = 1000 / 500;
+      notesOf500 = (amount - notesOf5000 * 5000 - notesOf1000 * 1000) / 500;
     } else {
       notesOf5000 = parseInt(amount / 5000) - 1;
       notesOf1000 = parseInt((amount - notesOf5000 * 5000) / 1000) - 1;
-      notesOf500 = 1000 / 500;
+      notesOf500 = (amount - notesOf5000 * 5000 - notesOf1000 * 1000) / 500;
     }
   } else {
     if (amount % 5000 !== 0 && amount % 1000 !== 0 && amount % 500 === 0) {
@@ -36,13 +37,26 @@ if (amount % 500 === 0 && amount > 0 && amount <= 20000) {
     "<h3>" +
       "You will have " +
       notesOf5000 +
-      " fiveThousand notes " +
+      " five Thousand notes " +
       notesOf1000 +
-      " oneThousand notes " +
+      " one Thousand notes " +
       notesOf500 +
-      " fiveHundred notes " +
+      " five Hundred notes " +
       "</h3>"
   );
 } else {
   document.write("<h2>Invalid Amount</h2>");
 }
+*/
+
+// Custom indexOf method
+var text = "Hello world";
+function customIndexOf(a) {
+  for (var i = 0; i < text.length; i++) {
+    if (a === text[i]) {
+      return i;
+    }
+  }
+  return i - text.length - 1;
+}
+console.log(customIndexOf("r"));
