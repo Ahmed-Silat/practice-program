018f09046ddfd4c8debccb9125ddfca439a09e1e
Ahmed-Silat <ahmedsilat@gmail.com>
2022-10-04 17:58:38 +0500

    practice

--- app.js
+++ app.js
@@ -4,9 +4,6 @@ var amount = Number(prompt("Enter the amount you want to withdraw", "0"));
 var notesOf5000;
 var notesOf1000;
 var notesOf500;
-// var fiveThousand = 0;
-// var oneThousand = 0;
-// var fiveHundred = 0;
 if (amount % 500 === 0 && amount > 0 && amount <= 20000) {
   if (amount % 5000 === 0) {
     notesOf5000 = parseInt(amount / 5000) - 1;
@@ -47,7 +44,6 @@ if (amount % 500 === 0 && amount > 0 && amount <= 20000) {
 } else {
   document.write("<h2>Invalid Amount</h2>");
 }
-
 
 // Custom indexOf method
 /*
