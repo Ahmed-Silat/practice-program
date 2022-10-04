788db7cd23ff011d3977b197368b2b9e4dcf111a
Ahmed-Silat <ahmedsilat@gmail.com>
2022-10-04 12:58:40 +0500

    sixth commit

--- app.js
+++ app.js
@@ -50,13 +50,58 @@ if (amount % 500 === 0 && amount > 0 && amount <= 20000) {
 */
 
 // Custom indexOf method
+/*
 var text = "Hello world";
-function customIndexOf(a) {
+function customIndexOf(param) {
   for (var i = 0; i < text.length; i++) {
-    if (a === text[i]) {
+    if (param === text[i]) {
       return i;
     }
   }
   return i - text.length - 1;
 }
 console.log(customIndexOf("r"));
+*/
+
+// Vowel Program
+var text = "Please read this application and give me gratuity";
+var array = text.split(" ");
+console.log(array);
+var pairOfVowels = [];
+var setOfVowels = [];
+function checkVowel(param) {
+  switch (param) {
+    case "a":
+      return true;
+    // break;
+    case "e":
+      return true;
+    case "i":
+      return true;
+    case "o":
+      return true;
+    case "u":
+      return true;
+    default:
+      return false;
+  }
+}
+
+for (var i = 0; i < array.length; i++) {
+  var word = array[i].split("");
+  for (var j = 0; j < word.length - 1; j++) {
+    var pair = word[j] + word[j + 1];
+    pairOfVowels.push(pair);
+  }
+}
+console.log(pairOfVowels);
+
+for (var i = 0; i < pairOfVowels.length; i++) {
+  var splittedPair = pairOfVowels[i].split("");
+  var firstLetter = splittedPair[0];
+  var secondLetter = splittedPair[1];
+  if (checkVowel(firstLetter) && checkVowel(secondLetter)) {
+    setOfVowels.push(pairOfVowels[i]);
+  }
+}
+console.log(setOfVowels);
