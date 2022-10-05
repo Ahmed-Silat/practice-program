1de40a50268ca2dc3b9553347c0d82570b471e6a
Ahmed-Silat <ahmedsilat@gmail.com>
2022-10-06 01:02:01 +0500

    change of function name

--- atm-program.js
+++ atm-program.js
@@ -3,21 +3,21 @@ var notesOf5000;
 var notesOf1000;
 var notesOf500;
 
-function generatingNotes() {
+function generatingMoney() {
   notesOf5000 = parseInt(amount / 5000) - 1;
   notesOf1000 = parseInt((amount - notesOf5000 * 5000) / 1000) - 1;
   notesOf500 = (amount - notesOf5000 * 5000 - notesOf1000 * 1000) / 500;
 }
 if (amount % 500 === 0 && amount > 0 && amount <= 20000) {
   if (amount % 5000 === 0) {
-    generatingNotes();
+    generatingMoney();
   } else if (amount % 5000 !== 0 && amount % 1000 === 0) {
     if (amount < 5000) {
       notesOf5000 = parseInt(amount / 5000);
       notesOf1000 = parseInt((amount - notesOf5000 * 5000) / 1000) - 1;
       notesOf500 = (amount - notesOf5000 * 5000 - notesOf1000 * 1000) / 500;
     } else {
-      generatingNotes();
+      generatingMoney();
     }
   } else {
     if (amount < 5000) {
